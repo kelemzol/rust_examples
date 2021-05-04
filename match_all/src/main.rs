ea33c1f2f82c3c7365330adfa9b2f91e752c6ecb
Sonor <ghsonor@gmail.com>
2021-05-04 17:54:11 +0200

    Match examples finished

--- match_all/src/main.rs
+++ match_all/src/main.rs
@@ -16,4 +16,10 @@ fn main() {
         Monsters::Bitbake(_) => println!("This is too terrifying"),
         _ => println!("We can deal with this") //Match all syntax. Should always be the last in a match loop
     }
+
+    if let dragon = Monsters::Dragon("Dragon".to_string()) { // Simpler, one line syntax to do the same thing as above
+        println!("Not even close to bitbake");
+    } else {
+        println!("I'm running out of jokes"); //Else works with if let syntax
+    }
 }
